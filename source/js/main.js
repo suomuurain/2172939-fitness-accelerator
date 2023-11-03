c18725ab602021c548db9e7269bbd9e32006c6f0
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-11-03 14:38:49 +0300

    добавляет табы

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 
+import {initTabs} from './vendor/tabs/init-tabs';
 import './modules/reviews-swiper';
 
 // ---------------------------------
@@ -23,6 +24,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+    initTabs();
   });
 });
 
