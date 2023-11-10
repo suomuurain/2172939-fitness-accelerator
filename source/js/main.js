aca26e89cbd50adf19ca85d9fb23129c73e46008
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-11-11 00:04:55 +0300

    валидация

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
-
 import {initTabs} from './vendor/tabs/init-tabs';
 import {initAccordions} from './vendor/accordions/init-accordion';
 import './modules/jury-swiper';
@@ -22,7 +20,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
     const form = new Form();
     window.form = form;
     form.init();
