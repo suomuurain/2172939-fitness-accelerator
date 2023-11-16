d4d2690e9c74b386790be4b3ac13716c7ebed83b
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-11-16 19:12:39 +0300

    откатывает версию

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -1,20 +1,20 @@
 function findVideos() {
-  const videos = document.querySelectorAll('.video');
+  let videos = document.querySelectorAll('.video');
 
-  for (const i = 0; i < videos.length; i++) {
+  for (let i = 0; i < videos.length; i++) {
     setupVideo(videos[i]);
   }
 }
 
 function setupVideo(video) {
-  const link = video.querySelector('.video__link');
-  const media = video.querySelector('.video__media');
-  const button = video.querySelector('.video__button');
-  const poster = video.querySelector('.video__poster');
-  const id = parseMediaUrl(media);
+  let link = video.querySelector('.video__link');
+  let media = video.querySelector('.video__media');
+  let button = video.querySelector('.video__button');
+  let poster = video.querySelector('.video__poster');
+  let id = parseMediaURL(media);
 
   video.addEventListener('click', () => {
-    const iframe = createIframe(id);
+    let iframe = createIframe(id);
 
     link.remove();
     button.remove();
@@ -26,27 +26,27 @@ function setupVideo(video) {
   video.classList.add('video--enabled');
 }
 
-function parseMediaUrl(media) {
-  const regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
-  const url = media.src;
-  const match = url.match(regexp);
+function parseMediaURL(media) {
+  let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+  let url = media.src;
+  let match = url.match(regexp);
 
   return match[1];
 }
 
 function createIframe(id) {
-  const iframe = document.createElement('iframe');
+  let iframe = document.createElement('iframe');
 
   iframe.setAttribute('allowfullscreen', '');
   iframe.setAttribute('allow', 'autoplay');
-  iframe.setAttribute('src', generateUrl(id));
+  iframe.setAttribute('src', generateURL(id));
   iframe.classList.add('video__media');
 
   return iframe;
 }
 
-function generateUrl(id) {
-  const query = '?rel=0&showinfo=0&autoplay=1';
+function generateURL(id) {
+  let query = '?rel=0&showinfo=0&autoplay=1';
 
   return 'https://www.youtube.com/embed/' + id + query;
 }
